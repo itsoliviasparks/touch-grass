1e38867058152611abb95f7b0678b56261e898e1
itsoliviasparks <itsoliviasparks@gmail.com>
2023-02-23 12:15:55 -0500

    added project repo link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
                 Created by <a href="https://itsoliviasparks.com/" target="_blank" rel="noreferrer" aria-label="link text - new window">Olivia Sparks</a> at <a href="https://junocollege.com" target="_blank" rel="noreferrer" aria-label="link text - new window">Juno College</a>. Data provided by <a href="https://www.nps.gov/subjects/developer/api-documentation.htm" target="_blank" rel="noreferrer" aria-label="link text - new window">The U.S. National Park Service</a>.
             </p>
             <p>
-            Project Repo<a href="https://github.com/itsoliviasparks/touch-grass" target="_blank" rel="noreferrer" aria-label="link text - new window">Olivia Sparks</a>
+            <a href="https://github.com/itsoliviasparks/touch-grass" target="_blank" rel="noreferrer" aria-label="link text - new window">Project Repo</a>
             </p>
         </footer>
     );
