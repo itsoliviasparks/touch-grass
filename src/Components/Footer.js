eb8c9cb2f5d3cf4a7221532a1a58adccb9900c1b
itsoliviasparks <itsoliviasparks@gmail.com>
2023-02-23 12:15:12 -0500

    added project repo link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,12 @@
 const Footer = () => {
     return (
         <footer>
-            <p>Created by <a href="https://itsoliviasparks.com/" target="_blank" rel="noreferrer" aria-label="link text - new window">Olivia Sparks</a> at <a href="https://junocollege.com" target="_blank" rel="noreferrer" aria-label="link text - new window">Juno College</a>. Data provided by <a href="https://www.nps.gov/subjects/developer/api-documentation.htm" target="_blank" rel="noreferrer" aria-label="link text - new window">The U.S. National Park Service</a>.</p>
+            <p>
+                Created by <a href="https://itsoliviasparks.com/" target="_blank" rel="noreferrer" aria-label="link text - new window">Olivia Sparks</a> at <a href="https://junocollege.com" target="_blank" rel="noreferrer" aria-label="link text - new window">Juno College</a>. Data provided by <a href="https://www.nps.gov/subjects/developer/api-documentation.htm" target="_blank" rel="noreferrer" aria-label="link text - new window">The U.S. National Park Service</a>.
+            </p>
+            <p>
+            Project Repo<a href="https://github.com/itsoliviasparks/touch-grass" target="_blank" rel="noreferrer" aria-label="link text - new window">Olivia Sparks</a>
+            </p>
         </footer>
     );
 };
