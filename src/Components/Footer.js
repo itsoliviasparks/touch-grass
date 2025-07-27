5ff7787d173176c32aacb93d86eb58f7a6eb9416
itsoliviasparks <97206055+itsoliviasparks@users.noreply.github.com>
2025-07-27 13:12:28 -0600

    updating to Firebase hosting & updating URL

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <p>
-                Created by <a href="https://itsoliviasparks.com/" target="_blank" rel="noreferrer" aria-label="link text - new window">Olivia Sparks</a> at <a href="https://junocollege.com" target="_blank" rel="noreferrer" aria-label="link text - new window">Juno College</a>. Data provided by <a href="https://www.nps.gov/subjects/developer/api-documentation.htm" target="_blank" rel="noreferrer" aria-label="link text - new window">The U.S. National Park Service</a>.
+                Created by <a href="https://github.com/itsoliviasparks" target="_blank" rel="noreferrer" aria-label="link text - new window">Olivia Sparks</a> at <a href="https://junocollege.com" target="_blank" rel="noreferrer" aria-label="link text - new window">Juno College</a>. Data provided by <a href="https://www.nps.gov/subjects/developer/api-documentation.htm" target="_blank" rel="noreferrer" aria-label="link text - new window">The U.S. National Park Service</a>.
             </p>
             <p>
             <a href="https://github.com/itsoliviasparks/touch-grass" target="_blank" rel="noreferrer" aria-label="link text - new window">Project Repo</a>
